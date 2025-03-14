f72c5e86966a6d1ef202832df9c5ac20e2f83cab
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-03-14 07:24:08 -0500

    Updated Entradas

--- app/Http/Controllers/api/UpdateController.php
+++ app/Http/Controllers/api/UpdateController.php
@@ -76,12 +76,12 @@ class UpdateController extends Controller
 
         if (isset($datos_json["entradas"]))
         {
-          //   return response()->json(
-          // [
-          //     'status' => '200',
-          //     'msg' => 'Actualización Exitosa',
-          //     'data' => "Estoy en la Entradas",
-          // ],Response::HTTP_ACCEPTED); 
+            return response()->json(
+          [
+              'status' => '200',
+              'msg' => 'Actualización Exitosa',
+              'data' => "Estoy en la Entradas",
+          ],Response::HTTP_ACCEPTED); 
 
             $entradas = $datos_json["entradas"];
             DB::statement('SET FOREIGN_KEY_CHECKS=0;');
