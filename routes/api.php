1d358d2720ae1b50ff8ea255960cd53bc357fe40
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-03-13 19:40:22 -0500

    Updated Login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +8,10 @@ use Illuminate\Support\Facades\Route;
 Route::get("/test", function () {
     return response()->json(["message" => "Hello World!"], 200);
 });
+
+Route::post('register', [AuthController::class,'register']);
+Route::post('login', [AuthController::class,'login']);
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
