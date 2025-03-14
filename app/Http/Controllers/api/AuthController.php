fb81cbb168dd9ac2ef56ff08b1653b8eaa922951
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-03-13 19:53:43 -0500

    Updated Login

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -4,16 +4,25 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\JsonResponse;
+//use GuzzleHttp\Psr7\Response;
+//use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 
 class AuthController extends Controller
 {
     public function login(Request $request):JsonResponse
     {
       //$correo = $request->email ."--" . $request->password;
-      $usuario = User::where('email','=',$request->email)->first();
+
+      return response()->json(
+        [
+          'status'      => '200 OK',
+          'msg'         => 'Voy Aquí',         
+        ],Response::HTTP_ACCEPTED);
 
 
         $request->validate([
