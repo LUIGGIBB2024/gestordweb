9dd87a5ae43aeb106802bb566fc85bb9e7e024d3
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-03-19 14:02:45 -0500

    Updated Entradas

--- app/Http/Controllers/api/GetEntradasController.php
+++ app/Http/Controllers/api/GetEntradasController.php
@@ -11,24 +11,8 @@ class GetEntradasController extends Controller
 {
     public function GetEntradas(Request $request):JsonResponse
     {
-        //$entradas   = $request->entradas;
-
-        return response()->json(
-            [
-                'status' => '200',
-                'msg' => 'Actualización Exitosa',
-                //'data' =>  $hasta . " - " . $desde,
-            ],Response::HTTP_ACCEPTED);
-
         $desde      = $request->desdefecha;
         $hasta      = $request->hastafecha;
-
-        return response()->json(
-            [
-                'status' => '200',
-                'msg' => 'Actualización Exitosa',
-                'data' =>  $hasta . " - " . $desde,
-            ],Response::HTTP_ACCEPTED);
 
         $consulta = \App\Models\EntradaDeEquipo::where('fechadereporte','>=',$desde)
                     ->where('fechadereporte','<=',$hasta)                                  
