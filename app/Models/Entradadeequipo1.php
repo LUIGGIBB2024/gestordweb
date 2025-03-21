7c21b5548cbe24b55ad9191f92e8cb3a5722f78d
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-03-20 23:47:35 -0500

    Updated Entradas

--- app/Models/Entradadeequipo1.php
+++ app/Models/Entradadeequipo1.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class ZEntradadeequipo extends Model
 {
     use HasFactory;
-    protected $table =  'entradadeequipos';
+    protected $table =  'entradadeequipos1';
 
     protected $fillable = [
         'tipodedocumento',
