78116713ace47a0a65ad9f7ea6535b4cb2e2c143
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-03-27 04:37:15 -0500

    Updated Miscelaneos

--- app/Http/Controllers/api/UpdateMiscelaneosController.php
+++ app/Http/Controllers/api/UpdateMiscelaneosController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Miscelaneo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -11,17 +13,35 @@ class UpdateMiscelaneosController extends Controller
 {
     //
     public function UpdateMiscelaneos(Request $request):JsonResponse
-    {
-        return response()->json(
-              [
-                  'status' => '200',
-                  'msg' => 'Actualización Exitosa',
-                  'data' =>  $request->fabricantes
-              ],Response::HTTP_ACCEPTED);   
-            // [
-            //     'status' => '405',
-            //     'msg'  => 'Imposible la Actualización',
-            //     'data' =>  "Apertura Fabricantes OK",
-            // ],Response::HTTP_BAD_REQUEST);
+    {        
+        if ($request->miscelaneos == null)
+           {
+             return response()->json(
+               [
+                 'status' => '405',
+                 'msg'  => 'Imposible la Actualización',
+                 'data' =>  "Apertura Miscelaneos - Sin Datos",
+                 ],Response::HTTP_BAD_REQUEST);
+           }
+
+           $miscelaneos = ($request->miscelaneos);
+           foreach ($miscelaneos as $dato)
+           {
+             $codigo = $dato["codigo"];
+
+             $miscelaneo = Miscelaneo::updateOrCreate(['codigo'=>$codigo],
+             [
+               'descripcion'           => $dato["descripcion"],
+               'estado'                => $dato["estado"],
+               'usuario_created'       =>Auth::user()->codigo,
+               'usuario_updated'       =>Auth::user()->codigo
+             ]);
+           }
+           return response()->json(
+             [
+              'status' => '200',
+              'msg' => 'Actualización Exitosa - Miscelaneos',     
+              'data' =>  $miscelaneos        
+             ],Response::HTTP_ACCEPTED);
     }
 }
